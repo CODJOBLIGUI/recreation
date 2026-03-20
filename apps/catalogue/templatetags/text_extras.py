023d42da35e5de8e320f8b46b0461052b5b6c5d0
CODJOBLIGUI <codjobligui@gmail.com>
2026-03-20 12:42:05 +0100

    Decode double-escaped HTML entities in resumes

--- apps/catalogue/templatetags/text_extras.py
+++ apps/catalogue/templatetags/text_extras.py
@@ -9,3 +9,11 @@ def unescape_html(value):
     if value is None:
         return ""
     return html.unescape(str(value))
+
+
+@register.filter
+def unescape_html_twice(value):
+    if value is None:
+        return ""
+    first = html.unescape(str(value))
+    return html.unescape(first)
